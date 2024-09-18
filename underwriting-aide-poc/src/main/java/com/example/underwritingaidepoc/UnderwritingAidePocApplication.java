92d530d7ab8137af8a988cf3c205b0ab4a8641d4
rupesh.chavan <rupesh.chavan@gravie.com>
2024-09-18 14:08:49 +0530

    initial commit

---
+++ underwriting-aide-poc/src/main/java/com/example/underwritingaidepoc/UnderwritingAidePocApplication.java
@@ -0,0 +1,13 @@
+package com.example.underwritingaidepoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnderwritingAidePocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnderwritingAidePocApplication.class, args);
+    }
+
+}
---
+++ underwriting-aide-poc/src/test/java/com/example/underwritingaidepoc/UnderwritingAidePocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.underwritingaidepoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnderwritingAidePocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
